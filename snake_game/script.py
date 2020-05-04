924b8f78372c544efaa6d7cca7e87f1fec658a05
Yegor Trussov <egortr2004@mail.ru>
2020-05-04 16:18:05 +0600

    level system added, new menues added

--- snake_game/script.py
+++ snake_game/script.py
@@ -18,20 +18,50 @@ class Node():
         self.x  = x
         self.y = y
 
-playerX = 7
-playerY = 7
+playerX = 10
+playerY = 10
 gridSize = 20
 tileC = 20
-appleX = 7
-appleY = 2
+appleX = 10
+appleY = 7
 vx = 0
 vy = 0
 trail = []
-tail = 10
+tail = 5
 score = 0
 direction = ''
 isPlaying = True
 isGameOver = False
+isNextLevelMenu = False
+isFinishGameMenu = False
+level = 1
+goalScore = 1
+levelNum = 3
+
+def reset():
+    global playerX; 
+    global playerY
+    global appleX
+    global appleY 
+    global trail 
+    global tail 
+    global score 
+    global vx 
+    global vy 
+    global isPlaying
+    global isGameOver 
+    
+    playerX = 10
+    playerY = 10
+    appleX = 10
+    appleY = 7
+    trail = []
+    tail = 5
+    vx = 0
+    vy = 0
+    score = 0
+    isPlaying = True 
+    isGameOver = False
 
 def drawApple():
     pygame.draw.rect(screen, [255, 0, 0], [
@@ -42,12 +72,34 @@ def drawApple():
     ])
 
 def drawGameOverMenu():
-    pygame.draw.rect(screen, [0, 255, 0], [70, 150, 260, 100])
+    pygame.draw.rect(screen, [0, 0, 255], [70, 150, 260, 100])
     pygame.draw.rect(screen, [0, 0, 0], [75, 155, 250, 90])
     gameOverText = font1.render('Game over, noob!', 1, (255, 255, 255))
     restertText = font.render('Press R to restart', 1, (255, 255, 255))
     screen.blit(gameOverText, (105, 165))
     screen.blit(restertText, (130, 205))
+
+def drawNextLevelMenu():
+    global level
+
+    pygame.draw.rect(screen, [0, 255, 0], [70, 60, 260, 300])
+    pygame.draw.rect(screen, [0, 0, 0], [75, 65, 250, 290])
+    gameOverText = font1.render('Congrats!', 1, (255, 255, 255))
+    passedLevelText = font.render('You have passed level ' + str(level) , 1, (255, 255, 255))
+    restertText = font.render('Press Space to continue!', 1, (255, 255, 255))
+    screen.blit(gameOverText, (145, 85))
+    screen.blit(passedLevelText, (100, 115))
+    screen.blit(restertText, (100, 300))
+
+def drawFinishGameMenu():
+    pygame.draw.rect(screen, [0, 255, 0], [70, 60, 260, 300])
+    pygame.draw.rect(screen, [0, 0, 0], [75, 65, 250, 290])
+    gameOverText = font1.render('Congrats!', 1, (255, 255, 255))
+    passedLevelText = font.render('You completed the game!', 1, (255, 255, 255))
+    restertText = font.render('Press Space to restart!', 1, (255, 255, 255))
+    screen.blit(gameOverText, (145, 85))
+    screen.blit(passedLevelText, (100, 115))
+    screen.blit(restertText, (100, 300))
 
 pygame.display.flip()
 
@@ -59,6 +111,24 @@ while (running):
             exit()
     
     keys = pygame.key.get_pressed()
+
+    if (isGameOver and keys[pygame.K_r]):
+        reset()
+        print(playerX)
+        isGameOver = False
+        isPlaying = True
+    
+    if (isNextLevelMenu and keys[pygame.K_SPACE]):
+        reset()
+        level += 1
+        isNextLevelMenu = False
+        isPlaying = True
+    
+    if (isFinishGameMenu and keys[pygame.K_SPACE]):
+        reset()
+        level = 1
+        isFinishGameMenu = False
+        isPlaying = True
 
     if (isPlaying):
 
@@ -79,6 +149,13 @@ while (running):
             vx = 1
             vy = 0
     
+    if (score >= goalScore):
+        isPlaying = False 
+        if (level == levelNum):
+            isFinishGameMenu = True
+        else:
+            isNextLevelMenu = True
+    
     if isPlaying:
         playerX += vx
         playerY += vy 
@@ -87,8 +164,10 @@ while (running):
     pygame.draw.rect(screen, [255, 0, 0], [0, 0, 400, 400])
     pygame.draw.rect(screen, [0, 0, 0], [5, 5, 390, 390])
 
-    scoreText = font.render(('Score: ' + str(score)), 1, (255, 255, 255))
+    scoreText = font.render(('Score: ' + str(score) + ' / ' + str(goalScore)), 1, (255, 255, 255))
+    levelText = font.render(('Level: ' + str(level)), 1, (255, 255, 255))
     screen.blit(scoreText, (10, 410))
+    screen.blit(levelText, (320, 410))
 
     if (playerX < 0):
         playerX = tileC - 1
@@ -103,7 +182,10 @@ while (running):
 
     for node in trail:
         index += 1
-        pygame.draw.rect(screen, [255, 0, 0], [
+        nodeColor = [255, 0, 0]
+        if (index == len(trail)):
+            nodeColor = [255, 255, 255]
+        pygame.draw.rect(screen, nodeColor, [
             node.x * gridSize,
             node.y * gridSize,
             gridSize - 2,
@@ -149,6 +231,12 @@ while (running):
     
     if (isGameOver):
         drawGameOverMenu()
+    
+    if (isNextLevelMenu):
+        drawNextLevelMenu()
+    
+    if (isFinishGameMenu):
+        drawFinishGameMenu()
     
     
     pygame.display.update()
